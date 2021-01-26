b5cdbf4bb3206ae06ed079b4204208cb55e8a54b
Pascal Berger <pascal.berger@gmail.com>
2021-01-26 22:33:36 +0100

    Add Frosting demo project

---
+++ demos/frosting/build/tasks/create-reports/CreateReportsHtmlDataTableTask.cs
@@ -0,0 +1,7 @@
+using Cake.Frosting;
+
+[TaskName("Create-Reports-HtmlDataTable")]
+// [IsDependentOn(typeof(WorldTask))]
+public class CreateReportsHtmlDataTableTask : FrostingTask
+{
+}
---
+++ demos/frosting/build/tasks/create-reports/CreateReportsHtmlDxDataGridTask.cs
@@ -0,0 +1,7 @@
+using Cake.Frosting;
+
+[TaskName("Create-Reports-HtmlDxDataGrid")]
+// [IsDependentOn(typeof(WorldTask))]
+public class CreateReportsHtmlDxDataGridTask : FrostingTask
+{
+}
